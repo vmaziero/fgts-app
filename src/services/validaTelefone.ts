00051a724168f7c0b176b0f21715c28435809142
vmaziero <victoriamaziero@alunos.utfpr.edu.br>
2025-05-05 23:29:23 -0300

    fix: Corrige API KEY que estava como undefined na req da validação de telefone.

--- src/services/validaTelefone.ts
+++ src/services/validaTelefone.ts
@@ -1,14 +1,19 @@
 export async function validaTelefone(telefone: string): Promise<boolean> {
-    const apiKey = import.meta.env.API_KEY;
+    const apiKey = import.meta.env.VITE_API_KEY;
     const url = `https://phonevalidation.abstractapi.com/v1/?api_key=${apiKey}&phone=${telefone}&country_code=BR`;
+
+    console.log(apiKey);
 
     try {
         const response = await fetch(url);
         const data = await response.json();
 
+        console.log(apiKey);
+        
         return data.valid === true;
     } catch (error) {
         console.error('Telefone inválido', error);
+        console.log(apiKey);
         return false;
     }
     
